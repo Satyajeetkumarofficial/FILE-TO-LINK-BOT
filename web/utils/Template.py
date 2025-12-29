2e3b45bfdf3cf78cdb453e8bbf6fd48c2634c499
Satyajeetkumarofficial <152394869+Satyajeetkumarofficial@users.noreply.github.com>
2025-12-29 13:41:03 +0530

    Update Template.py

--- web/utils/Template.py
+++ web/utils/Template.py
@@ -1,12 +1,12 @@
 class rexbots_template(object):
-    NAME = "RexBots Official"
+    NAME = "Pro Botz"
 
     DISCLAIMER = (
         "This platform provides high-speed streaming and download services. "
         "Strictly for personal use. Report any copyright infringements or inappropriate content to our support team."
     )
 
-    REPORT_LINK = "https://t.me/RexBots_Official"
+    REPORT_LINK = "https://t.me/QuickFile2LinkProBot"
 
     # Premium Color Palette (Dark Theme)
     COLOURS = {
