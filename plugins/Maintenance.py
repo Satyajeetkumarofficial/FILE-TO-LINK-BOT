731e68be587b0a033e0920d8f572ac7aa8364ff1
Satyajeetkumarofficial <152394869+Satyajeetkumarofficial@users.noreply.github.com>
2025-12-29 17:00:51 +0530

    Update Maintenance.py

--- plugins/Maintenance.py
+++ plugins/Maintenance.py
@@ -1,7 +1,6 @@
 import os
 from pyrogram import Client, filters
 from pyrogram.types import Message
-from pyrogram.enums import ParseMode
 from info import * 
 
 BYPASS_IDS = ADMINS + AUTH_CHANNEL + [BIN_CHANNEL, LOG_CHANNEL, PREMIUM_LOGS, VERIFIED_LOG]
@@ -12,12 +11,9 @@ async def maintenance_checker(client, message: Message):
     chat_id = message.chat.id
     if MAINTENANCE_MODE and user_id not in BYPASS_IDS and chat_id not in BYPASS_IDS:
         await message.reply(
-    "🚧 <b>Bot is under Maintenance!</b>\n\n"
-    "Please try again later.\n\n"
-    'Support Group: <a href="https://t.me/ProBotsDiscussionsGroup">➜ Open Chat</a>',
-    quote=True,
-    parse_mode=ParseMode.HTML
-)
+            "🚧 **Bot is under Maintenance!**\n\nPlease try again later.\n\nSupport Group: @ProBotUpdate",
+            quote=True
+        )
         return
 
     await message.continue_propagation()
