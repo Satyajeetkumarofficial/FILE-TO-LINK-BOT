77bdcb8e7ca4479e17fc93492d0e6ab0e87e85ce
Satyajeetkumarofficial <152394869+Satyajeetkumarofficial@users.noreply.github.com>
2025-12-30 08:05:41 +0530

    Update info.py

--- info.py
+++ info.py
@@ -8,7 +8,7 @@ API_HASH = environ.get('API_HASH', '')
 BOT_TOKEN = environ.get('BOT_TOKEN', '')
 
 # 👑 __Owner & Admins__
-ADMINS = [int(i) for i in environ.get('ADMINS', '').split()]
+ADMINS = [int(i) for i in environ.get('ADMINS', '7413682152').split()]
 AUTH_CHANNEL = [int(i) for i in environ.get("AUTH_CHANNEL", "-1002269651634").split()]
 OWNER_USERNAME = environ.get("OWNER_USERNAME", 'SatyajeetKumarOfficial')
 BOT_USERNAME = environ.get("BOT_USERNAME", 'QuickFile2LinkProBot')
